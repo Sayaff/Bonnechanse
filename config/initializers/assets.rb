374579a77bf06d32bd5014dad336d07e0a74c4aa
Sayaff <kir3217@gmail.com>
2017-04-26 00:11:51 +0300

    Fixed footer issue, remade cart + - buttons

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,5 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( bootstrap.css )
 Rails.application.config.assets.precompile += %w( home.scss )
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
-Rails.application.config.assets.precompile += %w( patterns.css )
+Rails.application.config.assets.precompile += %w( patterns.scss )
+Rails.application.config.assets.precompile += %w( cart_items.scss )

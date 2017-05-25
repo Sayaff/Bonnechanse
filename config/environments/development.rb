dbbcec01badf62e7e9e607661b7175f95b487742
Sayaff <kir3217@gmail.com>
2017-05-26 00:38:21 +0300

    fixing minor paperclip and catalog issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,10 +16,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-  :bucket => ENV["AWS_BUCKET"],
+  :bucket => 'bonnechanse-bucket',
   :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
   :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
-  :s3_region => ENV["AWS_REGION"]
+  :s3_region => 'eu-west-1'
   },
   :s3_host_name => "s3-eu-west-1.amazonaws.com", # Added entry
   :s3_domain_url => ":s3_host_name",

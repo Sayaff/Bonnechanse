416030bc32b54d93e83c8afadb3234249335b6d7
Sayaff <kir3217@gmail.com>
2017-06-14 15:26:17 +0300

    Initial dashboard build

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,15 @@
 class AdminController < ApplicationController
+before_action :admin_check
+
+layout "dashboard"
+
   def index
   end
+
+  private
+
+  def admin_check
+    redirect_to new_user_session_path unless user_signed_in? && current_user.admin?
+  end
+
 end

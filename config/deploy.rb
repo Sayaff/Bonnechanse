c39d9595b4a1553bcc2b47f9286c8d4c08e2d543
Sayaff <kir3217@gmail.com>
2017-07-13 20:53:16 +0300

    Delete tmp/cache from linked dirs in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :branch, :production
 set :deploy_to, '/home/syat/Bonnechanse'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, '2.3.1'

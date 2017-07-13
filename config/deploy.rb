21e04de5836709e94fea4153f20926d149019c69
Sayaff <kir3217@gmail.com>
2017-07-13 21:41:40 +0300

    Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :deploy_to, '/home/syat/Bonnechanse'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
-set :keep_releases, 5
+set :keep_releases, 1
 set :rvm_type, :user
 set :rvm_ruby_version, '2.3.1'
 
